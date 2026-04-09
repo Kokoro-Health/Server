0ca300a896adeb36424cfdb6356c47cd8fd5f194
Nijasc <github.moonshine694@passmail.net>
2026-04-09 14:09:42 +0200

    Renamed energy controller path

--- api/src/main/kotlin/health/kokoro/api/rest/energy/EnergyController.kt
+++ api/src/main/kotlin/health/kokoro/api/rest/energy/EnergyController.kt
@@ -14,7 +14,7 @@ import java.time.Instant
 
 @RestController
 @Validated
-@RequestMapping("/energyEntries")
+@RequestMapping("/energy")
 class EnergyController(
     private val getEnergyEntries: GetEnergyEntries,
     private val addEnergyEntry: AddEnergyEntry,
